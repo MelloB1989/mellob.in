1113368a16e3fe6a743f957a43166d5a4c73d18c
Kartik Deshmukh <63499572+MelloB1989@users.noreply.github.com>
2025-08-17 18:44:18 +0530

    fix

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -60,7 +60,7 @@ export function ProjectCard({
         )}
         {image && (
           <Image
-            src={`https://img-proxy.mellob.in/img?url=${encodeURIComponent(image)}`}
+            src={`https://img-proxy.mellob.in/img?url=${encodeURIComponent(image ?? "")}`}
             alt={title}
             width={500}
             height={300}
